d5c041e32ed9e47ff2c63332ddb13930dfcd69ac
Kanokon Chongnguluam <kanokorn.may@gmail.com>
2016-04-13 19:07:56 +0700

    show number 1 - 9 correct

--- spec/lcd_number_spec.js
+++ spec/lcd_number_spec.js
@@ -4,4 +4,58 @@ describe("LCD Number", function() {
         var lcd = new LCD(1);
         expect(lcd.display()).toEqual(expectedNumber);
     });
+
+    it("should display 0 correct", function() {
+        var expectedNumber = [" -- ", "|  |", "|  |", "    ", "|  |", "|  |", " -- "];
+        var lcd = new LCD(0);
+        expect(lcd.display()).toEqual(expectedNumber);
+    });
+
+    it("should display 2 correct", function() {
+        var expectedNumber = [" -- ", "   |", "   |", " -- ", "|   ", "|   ", " -- "];
+        var lcd = new LCD(2);
+        expect(lcd.display()).toEqual(expectedNumber);
+    });
+
+    it("should display 3 correct", function() {
+        var expectedNumber = [" -- ", "   |", "   |", " -- ", "   |", "   |", " -- "];
+        var lcd = new LCD(3);
+        expect(lcd.display()).toEqual(expectedNumber);
+    });
+
+    it("should display 4 correct", function() {
+        var expectedNumber = ["    ", "|  |", "|  |", " -- ", "   |", "   |", "    "];
+        var lcd = new LCD(4);
+        expect(lcd.display()).toEqual(expectedNumber);
+    });
+
+    it("should display 5 correct", function() {
+        var expectedNumber = [" -- ", "|   ", "|   ", " -- ", "   |", "   |", " -- "];
+        var lcd = new LCD(5);
+        expect(lcd.display()).toEqual(expectedNumber);
+    });
+
+    it("should display 6 correct", function() {
+        var expectedNumber = [" -- ", "|   ", "|   ", " -- ", "|  |", "|  |", " -- "];
+        var lcd = new LCD(6);
+        expect(lcd.display()).toEqual(expectedNumber);
+    });
+
+    it("should display 7 correct", function() {
+        var expectedNumber = [" -- ", "|", "|", " ", "|", "|", " "];
+        var lcd = new LCD(7);
+        expect(lcd.display()).toEqual(expectedNumber);
+    });
+
+    it("should display 8 correct", function() {
+        var expectedNumber = [" -- ", "|  |", "|  |", " -- ", "|  |", "|  |", " -- "];
+        var lcd = new LCD(8);
+        expect(lcd.display()).toEqual(expectedNumber);
+    });
+
+    it("should display 9 correct", function() {
+        var expectedNumber = [" -- ", "|  |", "|  |", " -- ", "   |", "   |", " -- "];
+        var lcd = new LCD(9);
+        expect(lcd.display()).toEqual(expectedNumber);
+    });
 });
